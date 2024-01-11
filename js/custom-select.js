f4de525e243ae4ca32453404431776bc98b952f5
Ilya Svintsov <xeri237@gmail.com>
2024-01-11 23:03:00 +0200

    new commit

--- js/custom-select.js
+++ js/custom-select.js
@@ -1,12 +1,37 @@
-document.querySelector('.custom-select__button').addEventListener('click', function(){
-    document.querySelector('.custom-select__list').classList.toggle('custom-select__list-visible');
+document.querySelectorAll(".dropdown").forEach(function (dropDownWrapper) {
+  const dropDownBtn = dropDownWrapper.querySelector(".custom-select__button");
+  const dropDownList = dropDownWrapper.querySelector(".custom-select__list");
+  const dropDownListItems = dropDownList.querySelectorAll(".custom-select__item.item");
+  const dropDownInput = dropDownWrapper.querySelector(".custom-select__input__hidden");
 
-})
+  dropDownBtn.addEventListener("click", function () {
+    dropDownList.classList.toggle("custom-select__list-visible");
+  });
 
-document.querySelectorAll('.custom-select__item.item').forEach(function(listItem){
-    listItem.addEventListener('click', function(){
-        document.querySelector('.custom-select__button').innerText = this.innerText;
-        document.querySelector('.custom-select__button').focus(); 
-        document.querySelector('.custom-select__input__hidden').value = this.dataset.value;
-    })
-})
+  // выбор элемента списка. Запомнить выбранное значение. Закрыть дропдаун
+  dropDownListItems.forEach(function (listItem) {
+    listItem.addEventListener("click", function (e) {
+      e.stopPropagation();
+      dropDownBtn.innerText = this.innerText;
+      dropDownBtn.focus();
+      dropDownInput.value = this.dataset.value;
+      dropDownList.classList.remove("custom-select__list-visible");
+    });
+  });
+
+  // клик снаружи дропдауна
+
+  document.addEventListener("click", function (e) {
+    if (e.target !== dropDownBtn) {
+      document;
+      dropDownList.classList.remove("custom-select__list-visible");
+    }
+  });
+
+  // Нажмите на Tab или Escape. Закрыть
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "tab" || e.key === "escape") {
+      dropDownList.classList.remove("custom-select__list-visible");
+    }
+  });
+});
